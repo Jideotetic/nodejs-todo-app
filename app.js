d523b261f479b73040ecc3400bf7b22ca4b83a5b
Jideotetic <jideotetic@gmail.com>
2024-11-12 06:52:32 +0100

    Added express-session library
    The express-session library is being used to
    set todos list per client to avoid every
    connected client from seeing the same todos

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 require("dotenv").config();
 const express = require("express");
 const { urlencoded } = require("express");
+const session = require("express-session");
 const todoRouter = require("./routes/todoRouter");
 
 const app = express();
 
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
 // SETUP TEMPLATE ENGINE
-// app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // SETUP STATIC ASSETS
 app.use(express.static("public"));
 
 // PARSE REQ BODY
+// app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
 // SETUP ROUTING
